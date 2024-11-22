660179076506b8274fd03fa8931216fb90f36a3e
Marco <caballero.lechon@gmail.com>
2024-11-22 12:48:50 +0100

    añadido algo de metodo de comprobar disparo

--- HundirLaFlota/src/app/app.component.ts
+++ HundirLaFlota/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Coordenadas } from './models/Cordenadas';
+import { Barcos } from './models/Barcos';
+import { Flota } from './models/Flota';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +13,32 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HundirLaFlota';
-}
+  cordDisparo = new Array<Coordenadas>; 
+  barco: Barcos;
+
+  constructor() {
+    this.cordDisparo = [
+      { x: 1, y: 2 },
+      { x: 1, y: 3 }
+    ];
+  
+    this.barco = new Barcos(2, 0, false, [
+      { x: 1, y: 2 },
+      { x: 1, y: 3 }
+    ]);
+  }
+
+
+  comprobarDisparo(cordDisparo: Coordenadas, barco: Barcos) {
+    
+      barco.coordenadasBarco.forEach((Coordenadas) => {
+          if(cordDisparo === Coordenadas) {
+              
+          }
+      });
+        
+      }
+    
+  }
+
+
--- HundirLaFlota/src/app/models/Barcos.ts
+++ HundirLaFlota/src/app/models/Barcos.ts
@@ -1,14 +1,42 @@
 import { Coordenadas } from "./Cordenadas";
 
 export class Barcos {
-    tamano: number;
-    tocado: number;
-    cordenadasBarco: Set<Coordenadas>;
+    private _tamano: number;
+    private _tocado: number;
+    private _hundido: boolean;
+    private _cordenadasBarco: Coordenadas[];
 
-    constructor(tamano: number, tocado: number, cordenadasBarco: Set<Coordenadas>) {
-        this.tamano = tamano;
-        this.tocado = tocado;
-        this.cordenadasBarco = cordenadasBarco;
+    constructor(_tamano: number, _tocado: number, _hundido: boolean, _cordenadasBarco:Coordenadas[]) {
+        this._tamano = this.tamano;
+        this._tocado = this.tocado;
+        this._cordenadasBarco = _cordenadasBarco;
+        this._hundido = false;
     }
+
+    get tamano(): number {
+        return this._tamano;
+      }
+    
+      get tocado(): number {
+        return this._tocado;
+      }
+    
+      get hundido(): boolean {
+        return this._hundido;
+      }
+    
+      get coordenadasBarco(): Coordenadas[] {
+        return this._cordenadasBarco;
+      }
+    
+
+    marcarTocado() {
+        this._tocado++;
+        this._hundido = this.tocado === this.tamano;
+      }
+
+      estaHundido(): boolean {
+        return this.hundido;
+      }
 }
 
