c3c496bbb90b1b45dbc7d2255e1c9f703ee29cb3
Marco <caballero.lechon@gmail.com>
2024-11-25 10:46:16 +0100

    fixed

---
+++ HundirLaFlota/src/app/main-component/main-component.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Flota } from '../models/Flota';
+
+@Component({
+  selector: 'app-main-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-component.component.html',
+  styleUrl: './main-component.component.sass'
+})
+export class MainComponentComponent {
+
+  
+
+  comprobarDisparo(x: number = 1,  y: number = 2,flota: Flota) {
+    flota.barcos.forEach(barco => {
+      barco.coordenadasBarco.forEach((coordenada: { x: any; y: any; }) => {
+          if (coordenada.x === x && coordenada.y === y) {
+              console.log("¡Impacto! Se ha alcanzado un barco en las coordenadas", x + " " + y);
+          }
+      });
+  });
+}
+
+
+}
