5af5c6ab638012fda0c765def26b4092b63cd989
Antonio <antonio1234asdf@gmail.com>
2024-11-27 13:44:34 +0100

    aaaa

--- HundirLaFlota/src/app/body-component/body-component.component.ts
+++ HundirLaFlota/src/app/body-component/body-component.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { Barcos } from '../models/barcos';  
+import { Coordenadas } from '../models/coordenadas';
 
 @Component({
   selector: 'app-body-component',
   standalone: true,
   imports: [],
   templateUrl: './body-component.component.html',
-  styleUrl: './body-component.component.sass'
+  styleUrls: ['./body-component.component.sass']
 })
-export class BodyComponentComponent{
+export class BodyComponent {
+  flota: Barcos[] = []; 
+  tamanoBarco: number = 0; 
+  coordenadas: Coordenadas[] = []; 
+  x: number = 0; 
+  y: number = 0; 
+  barcosMaximos: number = 5; 
 
+  agregarBarco() {
+    if (this.flota.length < this.barcosMaximos) {
+      let nuevoBarco = new Barcos(this.tamanoBarco, 0, false, this.coordenadas, [] );
+      this.flota.push(nuevoBarco);
+      this.tamanoBarco = 0;
+      this.coordenadas = [];
+    } else {
+      alert('No puedes agregar más de 5 barcos.');
+    }
+  }
+
+  agregarCoordenadas() {
+    this.coordenadas.push(new Coordenadas(this.x, this.y));
+    this.x = 0; 
+    this.y = 0;
+  }
 }
