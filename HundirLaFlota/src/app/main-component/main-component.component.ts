37482069078ffc7fb92395cb185d7e88c7682ddc
Marco <caballero.lechon@gmail.com>
2024-11-27 14:01:22 +0100

    logica disparo hecha

--- HundirLaFlota/src/app/main-component/main-component.component.ts
+++ HundirLaFlota/src/app/main-component/main-component.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Flota } from '../models/Flota';
+import { Barcos } from '../models/Barcos';
+import { Coordenadas } from '../models/Cordenadas';
+import { Casilla } from '../models/Casilla';
 
 @Component({
   selector: 'app-main-component',
@@ -8,19 +10,43 @@ import { Flota } from '../models/Flota';
   templateUrl: './main-component.component.html',
   styleUrl: './main-component.component.sass'
 })
+
 export class MainComponentComponent {
+  Flota = new Array<Barcos>;
+  Barco1 = new Barcos(1, 0, false, [{ x: 1, y: 2 }], []);
+  tablero: Casilla[][] = [[new Casilla, new Casilla, new Casilla],[new Casilla, new Casilla, new Casilla]];
 
-  
+  constructor() {
+    this.Flota = [this.Barco1];
+    this.tablero
 
-  comprobarDisparo(x: number = 1,  y: number = 2,flota: Flota) {
-    flota.barcos.forEach(barco => {
-      barco.coordenadasBarco.forEach((coordenada: { x: any; y: any; }) => {
-          if (coordenada.x === x && coordenada.y === y) {
-              console.log("¡Impacto! Se ha alcanzado un barco en las coordenadas", x + " " + y);
-          }
+  }
+
+  disparo(x: number, y: number): boolean {
+    let huboImpacto = false;
+    this.Flota.forEach(barco => {
+      barco.coordenadasBarco.forEach((coordenada) => {
+        console.log(coordenada);
+        console.log(x, y)
+        if (coordenada.x == x && coordenada.y == y) {
+          console.log("¡Impacto! Se ha alcanzado un barco en las coordenadas", x, y);
+          huboImpacto = true;
+        }
       });
-  });
-}
+    });
+    return huboImpacto;
+  }
 
+  realizarDisparo(x: number, y: number) { 
+    const impacto = this.disparo(x, y);
+    if (impacto) {
+      console.log("¡Has acertado!");
+      this.tablero[x][y].url = "boom.png";
+    } else {
+      console.log("¡Agua!");
+      this.tablero[x][y].url = "agua.png";
+    }
+
+  }
 
 }
